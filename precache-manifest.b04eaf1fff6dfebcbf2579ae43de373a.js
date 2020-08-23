291d2e13da1777c6b59546635dd8df43d2f01a96
Abhay Jirati <abhi.jrt12@gmail.com>
2020-08-23 13:25:36 +0530

    Updates

---
+++ precache-manifest.b04eaf1fff6dfebcbf2579ae43de373a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d749dbd20b315f42c55c2097ff18f56f",
+    "url": "/movie-app/index.html"
+  },
+  {
+    "revision": "4837d571554513b38bfd",
+    "url": "/movie-app/static/css/main.57fbc47d.chunk.css"
+  },
+  {
+    "revision": "ee65ff770bedd377a2f9",
+    "url": "/movie-app/static/js/2.9a5ab406.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie-app/static/js/2.9a5ab406.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4837d571554513b38bfd",
+    "url": "/movie-app/static/js/main.985f79ec.chunk.js"
+  },
+  {
+    "revision": "8c0fe60c15a349e095c4",
+    "url": "/movie-app/static/js/runtime-main.1fbae34b.js"
+  }
+]);
